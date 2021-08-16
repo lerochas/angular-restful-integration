ca933f679d64883042aa1c7ffebc55008fcd73bd
lerochas <emaildole.07@gmail.com>
2021-08-16 09:27:42 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatosListagemComponent } from './contatos-listagem/contatos-listagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatosListagemComponent
   ],
   imports: [
     BrowserModule,
